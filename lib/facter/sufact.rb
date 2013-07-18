76da3a5fb1d9fa99eb6bb067e1372bc14a54c4e8
John Jendro <johnj@cat.pdx.edu>
2013-07-18 12:35:20 -0700

    If the file exists but is zero size assume the fact is true

--- lib/facter/sufact.rb
+++ lib/facter/sufact.rb
@@ -6,10 +6,18 @@ if File.directory? "#{path}"
   Dir.foreach(path) do |entry|
     entry.chomp
     if File.file? "#{path}/#{entry}"
-      Facter.add(entry) do
-	setcode do
-	  contents = File.read("#{path}/#{entry}")
-	  contents.chomp
+      if File.zero? "#{path}/#{entry}"
+	Facter.add(entry) do
+	  setcode do
+	    contents = 'true'
+          end
+	end
+      else
+	Facter.add(entry) do
+	  setcode do
+	    contents = File.read("#{path}/#{entry}")
+	    contents.chomp
+	  end
 	end
       end
     end
