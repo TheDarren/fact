030bd90ebe2ff0a1a24b7405692bf9ee87fed195
Sage Imel <sage@cat.pdx.edu>
2013-10-24 11:04:36 -0700

    Change the names of the fact class and define

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,47 +1,26 @@
-# Provide arbitrary named facts - cannot match name of facter plugin (from
-# module path)
-#
-#
-# Example
-#
-#       fact { "su_sysadmin0": value => "SunetID" }
-#       fact { "su_sysadmin1": value => "SunetID2" }
-#       fact { "su_support": value => "IDG" }
-#       fact { "su_restricted": value => "true" }
-#
-
-define fact (
-  $ensure = present,
-  $value  = unset
+class fact(
+  $facterdir = '/etc/facter'
 ) {
-  include fact::setup
+  $factsdir = "${facterdir}/facts.d"
 
-  $factsdir = $fact::setup::factsdir
+  $root_group = $::id ? {
+    root    => 0,
+    default => $::id
+  }
 
-  $filepath = "${factsdir}/${name}.txt"
+  file { $facterdir:
+    ensure => directory,
+    owner  => $::id,
+    group  => $root_group,
+    mode   => '0755',
+  }
 
-  validate_string($value)
-
-  case $ensure {
-    absent: {
-      file { $filepath: ensure => absent }
-    }
-    present: {
-      case $value {
-        'NOSRC': {
-          fail 'value required for fact define'
-        }
-        default: {
-          file { $filepath:
-            content => inline_template('<%= @name %>=<%= @value %>'),
-            owner   => $::id,
-            group   => $fact::setup::root_group,
-            mode    => '0644',
-            require => File[$factsdir],
-          }
-        }
-      }
-    }
-    default: { crit "Invalid ensure value: ${ensure}." }
+  file { $factsdir:
+    ensure  => directory,
+    purge   => true,
+    recurse => true,
+    owner   => $::id,
+    group   => $root_group,
+    mode    => '0755',
   }
 }
---
+++ manifests/text.pp
@@ -0,0 +1,43 @@
+# Example
+#
+#       fact::text { "su_sysadmin0": value => "SunetID" }
+#       fact::text { "su_sysadmin1": value => "SunetID2" }
+#       fact::text { "su_support": value => "IDG" }
+#       fact::text { "su_restricted": value => "true" }
+#
+
+define fact::text (
+  $ensure = present,
+  $value  = unset
+) {
+  include fact
+
+  $factsdir = $fact::factsdir
+
+  $filepath = "${factsdir}/${name}.txt"
+
+  validate_string($value)
+
+  case $ensure {
+    absent: {
+      file { $filepath: ensure => absent }
+    }
+    present: {
+      case $value {
+        'NOSRC': {
+          fail 'value required for fact define'
+        }
+        default: {
+          file { $filepath:
+            content => inline_template('<%= @name %>=<%= @value %>'),
+            owner   => $::id,
+            group   => $fact::root_group,
+            mode    => '0644',
+            require => File[$factsdir],
+          }
+        }
+      }
+    }
+    default: { crit "Invalid ensure value: ${ensure}." }
+  }
+}
